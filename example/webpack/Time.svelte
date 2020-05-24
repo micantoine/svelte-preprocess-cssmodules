f781c15112f2789ff297a116861a216b66315571
Michael Vurchio <michael@micantoine.dev>
2020-05-24 12:58:53 +0900

    Add webpack example

---
+++ example/webpack/Time.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { onDestroy } from 'svelte'
+
+  let date = new Date()
+  $: time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+  
+  const interval = setInterval(() => date = new Date(), 1000)
+
+  onDestroy(() => {
+    clearInterval(interval)
+  })
+</script>
+
+<style>
+  div {
+    text-align: right;
+    font-size: 1.2rem;
+    font-family: monospace;
+  }
+</style>
+
+<div class="$style.datetime">{time}</div>
---
+++ example/webpack/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const cssModules = require('../../index');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: path.resolve(__dirname, 'main.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svelte$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'svelte-loader',
+            options: {
+              preprocess: [
+                cssModules({
+                  localIdentName: '[hash:base64:10]',
+                  includePaths: ['./']
+                })
+              ],
+              emitCss: false
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 9000
+  }
+};
