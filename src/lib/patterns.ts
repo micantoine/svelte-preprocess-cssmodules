6a49b4050692da72337518677b736f699b75e4b1
Michael Vurchio <michael@micantoine.dev>
2020-11-16 00:06:17 +0900

    add import pattern

--- src/lib/patterns.ts
+++ src/lib/patterns.ts
@@ -1,6 +1,8 @@
 export const PATTERN_MODULE = /\$(style)?\.(:?[\w\d-]*)/gm;
-export const PATTERN_STYLE = /<style(\s[^]*?)?>([^]*?)<\/style>/gi;
 export const PATTERN_PATH_UNALLOWED = /[<>:"/\\|?*]/g;
+export const PATTERN_STYLE = /<style(\s[^]*?)?>([^]*?)<\/style>/gi;
+export const PATTERN_IMPORT = /import\s*(?:(.+)\s+from\s+)['|"](.+s?css)['|"];?/gm;
+
 export const PATTERN_CLASSNAME = (className: string): RegExp =>
   new RegExp(`\\.(${className})\\b(?![-_])`, 'gm');
 export const PATTERN_CLASS_SELECTOR = (className: string): RegExp =>
