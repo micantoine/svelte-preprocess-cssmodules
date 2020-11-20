7e9db11f5c1a3a7bc7fd346c7ce9783571d3686a
Michael Vurchio <michael@micantoine.dev>
2020-11-20 12:47:15 +0900

    Do not consider commented imports

--- src/lib/patterns.ts
+++ src/lib/patterns.ts
@@ -1,7 +1,7 @@
 export const PATTERN_MODULE = /\$(style)?\.(:?[\w\d-]*)/gm;
 export const PATTERN_PATH_UNALLOWED = /[<>:"/\\|?*]/g;
 export const PATTERN_STYLE = /<style(\s[^]*?)?>([^]*?)<\/style>/gi;
-export const PATTERN_IMPORT = /import\s*(?:(.+)\s+from\s+)['|"](.+?(s?css))['|"];?/gm;
+export const PATTERN_IMPORT = /(?<!\/\/\s*)import\s*(?:(.+)\s+from\s+)?['|"](.+?(s?css))['|"];?/gm;
 export const PATTERN_CLASS_SELECTOR = /\.(?<className>.+?(?=\W)+)(?![-_])/g;
 
 export const PATTERN_CLASSNAME = (className: string): RegExp =>
