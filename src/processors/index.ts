d8d1319dc09b0552ead357bb00635d310e7cdb57
Michael Vurchio <michael@micantoine.dev>
2021-04-03 23:28:04 +0900

    export all processors

--- src/processors/index.ts
+++ src/processors/index.ts
@@ -1,2 +1,5 @@
 export { default as parseMarkup } from './parseMarkup';
 export { default as parseStyle } from './parseStyle';
+export { default as nativeProcessor } from './native';
+export { default as mixedProcessor } from './mixed';
+export { default as scopedProcessor } from './scoped';
