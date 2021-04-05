2ef33e4bf8a9ca7966c1faa608152a146807339c
Michael Vurchio <michael@micantoine.dev>
2021-04-05 23:48:13 +0900

    Parse simple import (with no specifiers)

--- src/processors/scoped.ts
+++ src/processors/scoped.ts
@@ -1,18 +1,24 @@
+import path from 'path';
+import fs, { constants } from 'fs';
 // @ts-expect-error walk is not in d.ts
-import { walk } from 'svelte/compiler';
+import { parse, walk } from 'svelte/compiler';
 import MagicString from 'magic-string';
-import type { Ast, TemplateNode } from 'svelte/types/compiler/interfaces.d';
+import type { Ast, TemplateNode, Style } from 'svelte/types/compiler/interfaces.d';
 import type { PluginOptions, CSSModuleList } from '../types';
 import { camelCase, createClassName } from '../lib';
 import parseTemplate from './parseTemplate';
 
 const cssModuleList: CSSModuleList = {};
+const style: { ast: Style; openTag: string; closeTag: string } = {
+  ast: null,
+  openTag: '<style module>',
+  closeTag: '</style>',
+};
 let processedFilename: string;
 let pluginOptions: PluginOptions;
 let unParsedContent: string;
-let magicContent: MagicString;
 
-const parseStyle = (ast: Ast): void => {
+const parseStyle = (ast: Ast, magicContent: MagicString): MagicString => {
   walk(ast, {
     enter(node: TemplateNode) {
       if (node.type === 'Script' || node.type === 'Fragment') {
@@ -31,6 +37,52 @@ const parseStyle = (ast: Ast): void => {
       }
     },
   });
+
+  return magicContent;
+};
+
+const parseImport = async (ast: Ast, magicContent: MagicString): Promise<MagicString> => {
+  walk(ast, {
+    enter(node: TemplateNode) {
+      if (node.type === 'Style' || node.type === 'Fragment') {
+        this.skip();
+      }
+      if (
+        node.type === 'ImportDeclaration' &&
+        node.source.value.search(/\.module\.s?css$/) !== -1
+      ) {
+        console.log(node);
+        const absolutePath = path.resolve(path.dirname(processedFilename), node.source.value);
+        const nodeModulesPath = path.resolve(`${path.resolve()}/node_modules`, node.source.value);
+
+        try {
+          const fileContent = fs.readFileSync(absolutePath, 'utf8');
+          const fileStyle = `${style.openTag}${fileContent}${style.closeTag}`;
+          if (node.specifiers.length === 0) {
+            let fileMagicContent = new MagicString(fileStyle);
+            fileMagicContent = parseStyle(
+              parse(fileStyle, { filename: absolutePath }),
+              fileMagicContent
+            );
+            magicContent.remove(node.start, node.end);
+            magicContent.prependLeft(
+              style.ast.content.start,
+              fileMagicContent.toString().replace(style.openTag, '').replace(style.closeTag, '')
+            );
+          } else {
+            console.log('todo');
+          }
+        } catch (err) {
+          fs.access(nodeModulesPath, constants.F_OK, (error) => {
+            if (error) {
+              throw new Error(err); // not found in node_modules packages either, throw orignal error
+            }
+          });
+        }
+      }
+    },
+  });
+  return magicContent;
 };
 
 const processor = async (
@@ -39,13 +91,23 @@ const processor = async (
   filename: string,
   options: PluginOptions
 ): Promise<string> => {
+  let magicContent = new MagicString(content);
+
   processedFilename = filename;
   pluginOptions = options;
   unParsedContent = content;
-  magicContent = new MagicString(content);
 
   if (ast.css) {
-    parseStyle(ast);
+    magicContent = parseStyle(ast, magicContent);
+  }
+
+  if (ast.instance) {
+    if (ast.css) {
+      style.ast = ast.css;
+      style.openTag = unParsedContent.substring(style.ast.start, style.ast.content.start);
+    }
+    magicContent = await parseImport(ast, magicContent);
+    console.log(magicContent.toString());
   }
 
   if (Object.keys(cssModuleList).length > 0) {
