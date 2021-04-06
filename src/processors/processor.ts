095fbfc92dc993a052b3bee9a11f9fa6e7e6d0ce
Michael Vurchio <michael@micantoine.dev>
2021-04-07 01:16:41 +0900

    Create Processor class

---
+++ src/processors/processor.ts
@@ -0,0 +1,100 @@
+import MagicString from 'magic-string';
+import type { Ast, Style } from 'svelte/types/compiler/interfaces.d';
+import { CSSModuleList, PluginOptions } from '../types';
+import { camelCase, createClassName, hasModuleAttribute, hasModuleImports } from '../lib';
+import parseImports from './parseImports';
+
+export default class Processor {
+  public filename: string;
+  public options: PluginOptions;
+  public rawContent: string;
+  public cssModuleList: CSSModuleList = {};
+  public importedCssModuleList: CSSModuleList = {};
+
+  public ast: Ast;
+  public style: {
+    ast: Style;
+    openTag: string;
+    closeTag: string;
+  } = { ast: null, openTag: '<style module>', closeTag: '</style>' };
+
+  public magicContent: MagicString;
+
+  public styleParser: (param: Processor) => void;
+  public isParsingImports = false;
+
+  constructor(
+    ast: Ast,
+    content: string,
+    filename: string,
+    options: PluginOptions,
+    parser: (param: Processor) => void
+  ) {
+    this.filename = filename;
+    this.options = options;
+    this.rawContent = content;
+    this.ast = ast;
+    this.magicContent = new MagicString(content);
+    this.styleParser = parser.bind(this);
+
+    if (ast.css) {
+      this.style.ast = ast.css;
+      this.style.openTag = content.substring(this.style.ast.start, this.style.ast.content.start);
+    }
+  }
+
+  /**
+   * Create CssModule classname
+   * @param name The raw classname
+   * @returns The generated module classname
+   */
+  public createModuleClassname = (name: string): string => {
+    return createClassName(
+      this.filename,
+      this.rawContent,
+      this.ast.css.content.styles,
+      name,
+      this.options
+    );
+  };
+
+  /**
+   * Add CssModule data to list
+   * @param name The raw classname
+   * @param value The generated module classname
+   */
+  public addModule = (name: string, value: string): void => {
+    this.cssModuleList[name] = value;
+  };
+
+  /**
+   * Add CssModule data to imports list
+   * @param name The raw classname
+   * @param value The generated module classname
+   */
+  public addImportedModule = (name: string, value: string): void => {
+    this.importedCssModuleList[camelCase(name)] = value;
+  };
+
+  /**
+   * Parse component
+   * @returns The CssModule updated component
+   */
+  public parse = (): string => {
+    if (hasModuleAttribute(this.ast)) {
+      this.isParsingImports = false;
+      this.styleParser(this);
+    }
+
+    if (hasModuleImports(this.rawContent)) {
+      this.isParsingImports = true;
+      parseImports(this);
+    }
+
+    // if (Object.keys(this.cssModuleList).length > 0) {
+    //   // magicContent = parseTemplate(ast, magicContent, cssModuleList);
+    // }
+
+    return this.magicContent.toString();
+  };
+}
--- src/processors/scoped.ts
+++ src/processors/scoped.ts
@@ -1,149 +1,36 @@
-import path from 'path';
-import fs, { constants } from 'fs';
 // @ts-expect-error walk is not in d.ts
-import { parse, walk } from 'svelte/compiler';
-import MagicString from 'magic-string';
-import type { Ast, TemplateNode, Style } from 'svelte/types/compiler/interfaces.d';
-import type { PluginOptions, CSSModuleList } from '../types';
-import { camelCase, createClassName, hasModuleAttribute, hasModuleImports } from '../lib';
-import parseTemplate from './parseTemplate';
+import { walk } from 'svelte/compiler';
+import type { Ast, TemplateNode } from 'svelte/types/compiler/interfaces.d';
+import type { PluginOptions } from '../types';
+import Processor from './processor';
 
-const cssModuleList: CSSModuleList = {};
-let importedCssModuleList: CSSModuleList = {};
-const style: { ast: Style; openTag: string; closeTag: string } = {
-  ast: null,
-  openTag: '<style module>',
-  closeTag: '</style>',
-};
-let processedFilename: string;
-let pluginOptions: PluginOptions;
-let unParsedContent: string;
-
-const parseStyle = (ast: Ast, magicContent: MagicString, imported = false): MagicString => {
-  walk(ast, {
+const parser = (processor: Processor) => {
+  walk(processor.ast, {
     enter(node: TemplateNode) {
       if (node.type === 'Script' || node.type === 'Fragment') {
         this.skip();
       }
       if (node.type === 'ClassSelector') {
-        const generatedClassName = createClassName(
-          processedFilename,
-          unParsedContent,
-          ast.css.content.styles,
-          node.name,
-          pluginOptions
-        );
-        if (imported) {
-          importedCssModuleList[camelCase(node.name)] = generatedClassName;
+        const generatedClassName = processor.createModuleClassname(node.name);
+        if (processor.isParsingImports) {
+          processor.addImportedModule(node.name, generatedClassName);
         }
-        cssModuleList[node.name] = generatedClassName;
-        magicContent.overwrite(node.start, node.end, `.${generatedClassName}`);
+        processor.addModule(node.name, generatedClassName);
+        processor.magicContent.overwrite(node.start, node.end, `.${generatedClassName}`);
       }
     },
   });
-
-  return magicContent;
 };
 
-const parseImport = (ast: Ast, magicContent: MagicString): MagicString => {
-  let importedContent = '';
-  walk(ast, {
-    enter(node: TemplateNode) {
-      if (node.type === 'Style' || node.type === 'Fragment') {
-        this.skip();
-      }
-      if (
-        node.type === 'ImportDeclaration' &&
-        node.source.value.search(/\.module\.s?css$/) !== -1
-      ) {
-        const absolutePath = path.resolve(path.dirname(processedFilename), node.source.value);
-        const nodeModulesPath = path.resolve(`${path.resolve()}/node_modules`, node.source.value);
-
-        try {
-          importedCssModuleList = {};
-          const fileContent = fs.readFileSync(absolutePath, 'utf8');
-          const fileStyle = `${style.openTag}${fileContent}${style.closeTag}`;
-
-          let fileMagicContent = new MagicString(fileStyle);
-          fileMagicContent = parseStyle(
-            parse(fileStyle, { filename: absolutePath }),
-            fileMagicContent,
-            true
-          );
-
-          if (node.specifiers.length === 0) {
-            magicContent.remove(node.start, node.end);
-          } else if (node.specifiers[0].type === 'ImportDefaultSpecifier') {
-            const specifiers = `const ${node.specifiers[0].local.name} = ${JSON.stringify(
-              importedCssModuleList
-            )}`;
-            magicContent.overwrite(node.start, node.end, specifiers);
-          } else {
-            const specifierNames = node.specifiers.map((item: TemplateNode) => {
-              return item.local.name;
-            });
-            const specifiers = `const { ${specifierNames.join(', ')} } = ${JSON.stringify(
-              importedCssModuleList
-            )}`;
-            magicContent.overwrite(node.start, node.end, specifiers);
-          }
-
-          const content = `\n${fileMagicContent
-            .toString()
-            .replace(style.openTag, '')
-            .replace(style.closeTag, '')}`;
-
-          if (style.ast) {
-            magicContent.appendLeft(style.ast.content.start, content);
-          } else {
-            importedContent += content;
-          }
-        } catch (err) {
-          fs.access(nodeModulesPath, constants.F_OK, (error) => {
-            if (error) {
-              throw new Error(err); // not found in node_modules packages either, throw orignal error
-            }
-          });
-        }
-      }
-    },
-  });
-
-  if (importedContent) {
-    magicContent.append(`\n${style.openTag}${importedContent}${style.closeTag}`);
-  }
-  return magicContent;
-};
-
-const processor = async (
+const scopedProcessor = async (
   ast: Ast,
   content: string,
   filename: string,
   options: PluginOptions
 ): Promise<string> => {
-  let magicContent = new MagicString(content);
-
-  processedFilename = filename;
-  pluginOptions = options;
-  unParsedContent = content;
-
-  if (hasModuleAttribute(ast)) {
-    magicContent = parseStyle(ast, magicContent);
-  }
-
-  if (hasModuleImports(content)) {
-    if (ast.css) {
-      style.ast = ast.css;
-      style.openTag = unParsedContent.substring(style.ast.start, style.ast.content.start);
-    }
-    magicContent = parseImport(ast, magicContent);
-  }
-
-  if (Object.keys(cssModuleList).length > 0) {
-    magicContent = parseTemplate(ast, magicContent, cssModuleList);
-  }
-
-  return magicContent.toString();
+  const processor = new Processor(ast, content, filename, options, parser);
+  const processedContent = processor.parse();
+  return processedContent;
 };
 
-export default processor;
+export default scopedProcessor;
