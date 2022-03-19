e42cd9f2414cd389825506a58b9915f3698580c5
Michael Vurchio <micantoine.dev@gmail.com>
2022-03-19 13:49:39 +0900

    Test

---
+++ test/globalFixtures/bindVariable.test.js
@@ -0,0 +1,42 @@
+const compiler = require('../compiler.js');
+
+const script = "<script>let color = 'blue';</script>";
+
+describe('Bind variable to CSS', () => {
+  test('root elements', async () => {
+    const output = await compiler({
+      source: `${script}<div>blue</div><div>red</div><style module>div{color:bind(color)}</style>`,
+    }, {
+      cssVariableHash: '123',
+    });
+
+    expect(output).toBe(
+      `${script}<div style="--color-123:{color};">blue</div><div style="--color-123:{color};">red</div><style module>:global(div){color:var(--color-123)}</style>`
+    );
+  });
+
+  test('root element with attributes', async () => {
+    const output = await compiler({
+      source: `${script}<div class="blue">blue</div><style module>.blue{color:bind(color)}</style>`,
+    }, {
+      cssVariableHash: '123',
+      localIdentName: '[local]-123',
+    });
+
+    expect(output).toBe(
+      `${script}<div class="blue-123" style="--color-123:{color};">blue</div><style module>:global(.blue-123){color:var(--color-123)}</style>`
+    );
+  });
+
+  test('root element with style attribute', async () => {
+    const output = await compiler({
+      source: `${script}<div style="display:block">blue</div><style module>div{color:bind(color)}</style>`,
+    }, {
+      cssVariableHash: '123',
+    });
+
+    expect(output).toBe(
+      `${script}<div style="--color-123:{color};display:block">blue</div><style module>:global(div){color:var(--color-123)}</style>`
+    );
+  });
+});
